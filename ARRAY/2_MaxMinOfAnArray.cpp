8f271494a3d9d8d8b79bba62ddcb3527456399e9
divyasingh23499 <70092850+divyasingh23499@users.noreply.github.com>
2020-11-16 13:19:39 +0530

    Regular Update

--- ARRAY/2_MaxMinOfAnArray.cpp
+++ ARRAY/2_MaxMinOfAnArray.cpp
@@ -2,122 +2,122 @@
 using namespace std;
 
 
-#define int  long long int 
+#define int  long long int
 #define fast ios_base::sync_with_stdio(false); cin.tie(NULL);
 
-struct Pair  
-{ 
-    int min; 
-    int max; 
-};  
-  
+struct Pair
+{
+    int min;
+    int max;
+};
 
-//finding min and maximum from an array 
-//this is basic function on the basis of iteration 
+
+//finding min and maximum from an array
+//this is basic function on the basis of iteration
 // no of comparisons : (2*n - 2);
-struct Pair getMinMax(int arr[], int n) 
-{ 
-    struct Pair minmax;      
-  
-    if (n == 1) 
-    { 
-        minmax.max = arr[0]; 
-        minmax.min = arr[0];      
-        return minmax; 
-    }  
+struct Pair getMinMaxMethod1(int arr[], int n)
+{
+    struct Pair minmax;
 
-    if (arr[0] > arr[1])  
-    { 
-        minmax.max = arr[0]; 
-        minmax.min = arr[1]; 
-    }  
+    if (n == 1)
+    {
+        minmax.max = arr[0];
+        minmax.min = arr[0];
+        return minmax;
+    }
+
+    if (arr[0] > arr[1])
+    {
+        minmax.max = arr[0];
+        minmax.min = arr[1];
+    }
     else
-    { 
-        minmax.max = arr[1]; 
-        minmax.min = arr[0]; 
-    }  
-      
-    for(int i = 2; i < n; i++) 
-    { 
-        if (arr[i] > minmax.max)      
+    {
+        minmax.max = arr[1];
+        minmax.min = arr[0];
+    }
+
+    for(int i = 2; i < n; i++)
+    {
+        if (arr[i] > minmax.max)
         {
         	minmax.max = arr[i];
-        } 
-              
-        else if (arr[i] < minmax.min)      
+        }
+
+        else if (arr[i] < minmax.min)
         {
         	minmax.min = arr[i];
-        } 
-    } 
-    return minmax; 
-} 
+        }
+    }
+    return minmax;
+}
 
 
-//this is recursive solution 
+//this is recursive solution
 // no od comparisons : (1.5 *n - 2)
-struct Pair getMinMax(int arr[], int low, int high) 
-{ 
-    struct Pair minmax, mml, mmr;      
-    int mid; 
-    
-    if (low == high) 
-    { 
-        minmax.max = arr[low]; 
-        minmax.min = arr[low];      
-        return minmax; 
-    }  
+struct Pair getMinMaxMethod2(int arr[], int low, int high)
+{
+    struct Pair minmax, mml, mmr;
+    int mid;
 
-    if (high == low + 1) 
-    {  
-        if (arr[low] > arr[high])  
-        { 
-            minmax.max = arr[low]; 
-            minmax.min = arr[high]; 
-        }  
+    if (low == high)
+    {
+        minmax.max = arr[low];
+        minmax.min = arr[low];
+        return minmax;
+    }
+
+    if (high == low + 1)
+    {
+        if (arr[low] > arr[high])
+        {
+            minmax.max = arr[low];
+            minmax.min = arr[high];
+        }
         else
-        { 
-            minmax.max = arr[high]; 
-            minmax.min = arr[low]; 
-        }  
-        return minmax; 
-    } 
-      
-    mid = (low + high) / 2;  
-    mml = getMinMax(arr, low, mid); 
-    mmr = getMinMax(arr, mid + 1, high);  
-      
-    // Compare minimums of two parts 
-    if (mml.min < mmr.min) 
+        {
+            minmax.max = arr[high];
+            minmax.min = arr[low];
+        }
+        return minmax;
+    }
+
+    mid = (low + high) / 2;
+    mml = getMinMaxMethod2(arr, low, mid);
+    mmr = getMinMaxMethod2(arr, mid + 1, high);
+
+    // Compare minimums of two parts
+    if (mml.min < mmr.min)
     {
     	minmax.min = mml.min;
-    } 
+    }
     else
     {
-    	minmax.min = mmr.min; 
-   	}     
-      
-    // Compare maximums of two parts 
-    if (mml.max > mmr.max) 
+    	minmax.min = mmr.min;
+   	}
+
+    // Compare maximums of two parts
+    if (mml.max > mmr.max)
     {
     	minmax.max = mml.max;
-   	} 
+   	}
     else
     {
     	minmax.max = mmr.max;
-    }        
-    return minmax; 
-} 
-  
+    }
+    return minmax;
+}
+
 
 signed main(){
 
 	fast ;
-	
-	int test ;
-	cin >> test ;
+
+	int test = 1;
+	//cin >> test ;
 	while (test--)
 	{
-		//input 
+		//input
 		int n ;
 		cin >> n ;
 
@@ -128,11 +128,10 @@ signed main(){
 		}
 
 		//processing part
+        struct Pair minmax = getMinMaxMethod2(arr, 0, n - 1);
 
-		struct Pair minmax = getMinMax(arr, 0, n - 1); 		
+		//output part
 
-		//output part 
-		
 		cout << minmax.min << " " << minmax.max << endl ;
 
 	}
--- ARRAY/3_kthMaxAndMin.cpp
+++ ARRAY/3_kthMaxAndMin.cpp
@@ -1,25 +1,47 @@
+/*
+    Put all your eggs in one basket
+    Author : Divya Singh
+*/
 #include<bits/stdc++.h>
 using namespace std;
 
 
-#define int  long long int 
+#define int  long long int
 #define fast ios_base::sync_with_stdio(false); cin.tie(NULL);
 
-int kthMinimumSortingMethod(int arr[] , int n , int k)
+//O(N * logN)
+int kthMinimumSortingMethod1(int arr[] , int n , int k)
 {
     sort(arr, arr + n);
     return (arr[k]);
 }
 
+//O(N * logK)
+int kthMinimumSortingMethod2(int arr[] , int n , int k)
+{
+    priority_queue<int>pq;
+    for (int i = 0  ; i < n ; i ++)
+    {
+        pq.push(arr[i]);
+        if ((int)pq.size() > k)
+        {
+			pq.pop();
+		}
+    }
+
+    return (pq.top());
+}
+
+
 signed main(){
 
 	fast ;
-	
+
 	int test ;
 	cin >> test ;
 	while (test--)
 	{
-		//input 
+		//input
 		int n , k ;
 		cin >> n >> k;
 
@@ -30,10 +52,10 @@ signed main(){
 		}
 
 		//processing part
-        int ans = kthMinimumSortingMethod(arr , n , k);
-		
-        //output part 
-	   cout << ans << endl;
+        int ans = kthMinimumSortingMethod1(arr , n , k);
+
+        //output part
+        cout << ans << endl;
 	}
 	return 0 ;
 }
