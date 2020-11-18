2db16933d866c1c930ccc2a30ac936733cb73eb0
divyasingh23499 <70092850+divyasingh23499@users.noreply.github.com>
2020-11-18 12:44:07 +0530

    regular Update

---
+++ ARRAY/7_oneCyclicRotation.cpp
@@ -0,0 +1,48 @@
+/*
+    Put all your eggs in one basket
+    Author : Divya Singh
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int  long long int
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+//O(n*longn + m*logm + n)
+void oneTimeCyclicRotation(int arr[] , int sizeOfArray)
+{   
+    int lastElement = arr[sizeOfArray - 1];
+    for (int i = sizeOfArray - 1; i >= 1; i--) 
+    {
+        arr[i] = arr[i - 1];
+    }
+    arr[0] = lastElement;
+}
+
+signed main(){
+
+    fast ;
+    int test ;
+    cin >> test;
+    while (test--)
+    {
+        //input part
+        int n ; 
+        cin >> n ;
+
+        int arr[n];
+        for (int i = 0 ;  i < n ;  i++)
+        {
+            cin >> arr[i];
+        }
+
+        //processing part
+        oneTimeCyclicRotation(arr , n);
+
+        //output part
+        for (int i = 0 ;  i < n ;  i++)
+        {
+            cout << arr[i] << " ";
+        }
+    }
+}
